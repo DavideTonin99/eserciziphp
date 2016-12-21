712aa2d89062cad5b3fd658ef1f2c9aca7b83668
davide <davide9935@gmail.com>
2016-12-21 20:44:11 +0100

    aggiunto il controllo del prodotto acquistato maggiormente

--- secondo_esercizio/listaacquisti.php
+++ secondo_esercizio/listaacquisti.php
@@ -2,15 +2,28 @@
     session_start();
 
     function print_acquisti() {
+        $pezzi_totali = 0;
+        $max_pezzi = 0;
+        $max_prodotto = "";
+
         foreach ($_SESSION["acquisti"] as $prodotto => $pezzi) {
             if ($pezzi != "") {
                 echo "Prodotto: ".$prodotto.", pezzi: ".$pezzi."<br>";
+                $pezzi_totali += $pezzi;
+                if ($pezzi > $max_pezzi) {
+                    $max_pezzi = $pezzi;
+                    $max_prodotto = $prodotto;
+                }
             }
             else {
                 unset($_SESSION["acquisti"][$prodotto]);
             }
         }
+        echo "Pezzi totali: ".$pezzi_totali."<br>";
+        echo "Prodotto acquistato maggiormente: ".$max_prodotto.", pezzi: ".$max_pezzi."<br>";
+
     }
+
 ?>
 
 <html>
